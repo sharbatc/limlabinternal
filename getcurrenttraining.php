444570ea5e4e5732038dc2cd72886c054d0e3424
Sharbatanu Chatterjee <sharbatc@U281C>
2019-07-11 13:37:30 +0100

    add current training code

---
+++ getcurrenttraining.php
@@ -0,0 +1,23 @@
+ <?php
+  $servername = "172.24.155.100";
+  $username = "akrami";
+  $password = "Akrami2019!";
+  $dbname = "akrami_db";
+  $db = new mysqli($servername, $username, $password, $dbname);//set your database handler
+  $query = "SELECT * FROM sess_started where sessiondate like CURDATE() and was_ended like 0;";
+  $result = $db->query($query);
+
+  echo "<table class=\"table-hover table-bordered table-striped\"><tr><th class = \"text-justify\">Rig Name</th><th>Ratname</th><th>Date</th><th>Start Time</th><th></tr>";
+  while ($row = $result->fetch_assoc()){
+  	echo "<tr>"; 
+  	echo "<td>" . $row['hostname'] . "</td>";
+  	echo "<td>" . $row['ratname'] . "</td>";
+  	echo "<td>" . $row['sessiondate'] . "</td>";
+  	echo "<td>" . $row['starttime'] . "</td>";
+  	echo "</tr>";
+ }
+
+ echo "</table>";
+
+ mysqli_close($db);
+ ?> 

31584d3f199a22784a907761b5e3db63d437fece
Sharbatanu Chatterjee <sharbatc@U281C>
2019-07-11 14:37:08 +0100

    added beautificatio

--- getcurrenttraining.php
+++ getcurrenttraining.php
@@ -7,9 +7,8 @@
   $query = "SELECT * FROM sess_started where sessiondate like CURDATE() and was_ended like 0;";
   $result = $db->query($query);
   $date = $db->query("SELECT CURDATE();");
-  echo $date;
   echo "<div class=\"col-sm-4\">";
-  echo "<h4 class= \"text-center\"> Date : "."</h4>";
+  echo "<h4 class= \"text-center\"> Date : ".$date->fetch_assoc()."</h4>";
   echo "<table class=\"table-hover table-bordered table-striped\"><tr><th class = \"text-justify\">Rig Name</th><th>Ratname</th><th>Start Time</th><th></tr>";
   while ($row = $result->fetch_assoc()){
   	echo "<tr>"; 
